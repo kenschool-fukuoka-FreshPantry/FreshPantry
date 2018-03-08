5bdee75bf13d75b57608f4dbfcb3c171995847ae
ma-akahoshi <manamiakahoshi@gmail.com>
2018-03-08 14:17:23 +0900

    登録内容変更画面修正 aka

--- freshPantry/src/fPServlet/EditMenuService.java
+++ freshPantry/src/fPServlet/EditMenuService.java
@@ -29,15 +29,11 @@ public class EditMenuService extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
 	response.setContentType("text/html;charset=UTF-8");
 	String upDateMenuId = request.getParameter("update_menu_id");
 	RequestDispatcher rd = null;
 	String url = "";
-
-	{
 
 	// 変更メニューIDによって遷移画面を決定する。
 	try {
