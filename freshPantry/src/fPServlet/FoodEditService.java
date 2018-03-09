3a1aabdd3498fcbfae9241f7606adfb505c10b03
y-morikawa <twin_peaks@live.jp>
2018-03-09 16:20:30 +0900

    3.9 食材選択・食材変更サービス　森川

--- freshPantry/src/fPServlet/FoodEditService.java
+++ freshPantry/src/fPServlet/FoodEditService.java
@@ -1,6 +1,5 @@
 package fPServlet;
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -13,16 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import fPBean.FoodBean;
 
 /**
- * Servlet implementation class FoodDeleteService
+ * Servlet implementation class FoodEditService
  */
-@WebServlet("/FoodDeleteService")
-public class FoodDeleteService extends HttpServlet {
+@WebServlet("/FoodEditService")
+public class FoodEditService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public FoodDeleteService() {
+    public FoodEditService() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,13 +33,22 @@ public class FoodDeleteService extends HttpServlet {
 		// TODO Auto-generated method stub
 
 
-		//jspのラジオボタンで選択された項目を取得
+
+		//入力された項目の情報を取得
 		request.setCharacterEncoding("UTF-8");
-		int food_no = Integer.parseInt(request.getParameter("食材リスト"));
+		String food_name = (String)request.getParameter("food_name");
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		String purchase_date = (String)request.getParameter("purchase_date");
+		String expiration_date = (String)request.getParameter("expiration_date");
+		String category_id = (String)request.getParameter("category_id");
 
 		//Beanへ格納
 		FoodBean fb = new FoodBean();
-		fb.setFoodNo(food_no);
+		fb.setFoodName(food_name);
+		fb.setQuantity(quantity);
+		fb.setPurchaseDate(purchase_date);
+		fb.setExpirationDate(expiration_date);
+		fb.setCategoryId(category_id);
 
 
 		response.setContentType("text/html;charset=UTF-8");
@@ -49,35 +57,28 @@ public class FoodDeleteService extends HttpServlet {
 		RequestDispatcher rd = null;
 		String url = "";
 
-			if(buttonOnValue.equals("1")){
-				request.setAttribute("result", buttonOnValue);
-				// 1だったら、削除をよんで、元画面に戻る
-				request.setAttribute("message", "削除ボタンおしました。");
 
-				url = "FoodSelect.jsp";
+		if(buttonOnValue.equals("1")){
+			request.setAttribute("result", buttonOnValue);
+			// 1であれば、変更確認画面へ遷移
 
-			}else if(buttonOnValue.equals("2")){
-				request.setAttribute("result", buttonOnValue);
-				// 2だったら、次画面をよぶ
+			url = "FoodEditConfirm.jsp";
 
-				url = "FoodEdit.jsp";
+		}else{
+			request.setAttribute("result",buttonOnValue);
+			// それ以外であれば前画面に戻る
 
-			}else{
-				request.setAttribute("result",buttonOnValue);
-				// それ以外であれば前画面に戻る
-
-				url = "Edit.jsp";
+			url = "FoodSelect.jsp";
 
 
-				request.setAttribute("foodList", fb);
+			request.setAttribute("foodList", fb);
 
-				// 最後にフォワード
-				rd = request.getRequestDispatcher(url);
-				rd.forward(request, response);
-				return;
+			// 最後にフォワード
+			rd = request.getRequestDispatcher(url);
+			rd.forward(request, response);
+			return;
 
-			}
-
+		}
 	}
 
 	/**
