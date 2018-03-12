f9889308c0831fabecbc99e746def9467dd31cbe
ka-nakayama <nekonote.company.nakayama@gmail.com>
2018-03-12 12:58:59 +0900

    カテゴリと単位のＢｅａｎをアップしました（中山）

--- freshPantry/src/fPBean/CategoryBean.java
+++ freshPantry/src/fPBean/CategoryBean.java
@@ -1,5 +1,14 @@
 package fPBean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 /**
  * @author 中山佳代子
  *
@@ -29,9 +38,41 @@ public class CategoryBean {
 		this.category_name = category_name;
 	}
 
+	public static ArrayList<CategoryBean> getCategoryIdList() throws SQLException {
+		ArrayList<CategoryBean> al = new ArrayList<CategoryBean>();
+		InitialContext ic = null;
+		DataSource ds = null;
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		String sql = null;
 
-	/** ここから先は今から作成する予定 **/
-	public void getCategoryIdList() {
+		try{
+			ic = new InitialContext();
+			ds = (DataSource)ic.lookup("java:comp/env/mysql");
+			con = ds.getConnection();
+			sql = "SELECT * FROM CATEGORY ORDER BY category_id ASC";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
+
+			while(rs.next()){
+				CategoryBean c = new CategoryBean();
+				c.setCategoryId(rs.getString("category_id"));
+				c.setCategoryName(rs.getString("category_name"));
+				al.add(c);
+			}
+		}catch(Exception e){
+
+		}finally{
+			try{
+				if(rs != null) rs.close();
+				if(pst != null) pst.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e){
+			}
+		}
+		return al;
 
 	}
 
--- freshPantry/src/fPBean/UnitBean.java
+++ freshPantry/src/fPBean/UnitBean.java
@@ -1,5 +1,14 @@
 package fPBean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 /**
  * @author 中山佳代子
  *
@@ -29,20 +38,40 @@ public class UnitBean {
 		this.unit_name = unit_name;
 	}
 
-	/** ここから先は今から作成する予定 **/
-	public void addRecipData() {
+	public static ArrayList<UnitBean> getUnitIdList() throws SQLException {
+		ArrayList<UnitBean> al = new ArrayList<UnitBean>();
+		InitialContext ic = null;
+		DataSource ds = null;
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		String sql = null;
 
-	}
-	public void getRecipData() {
+		try{
+			ic = new InitialContext();
+			ds = (DataSource)ic.lookup("java:comp/env/mysql");
+			con = ds.getConnection();
+			sql = "SELECT * FROM UNIT ORDER BY unit ASC";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
 
-	}
-	public void getRecipList() {
+			while(rs.next()){
+				UnitBean u = new UnitBean();
+				u.setUnit(rs.getString("unit"));
+				u.setUnitName(rs.getString("unit_name"));
+				al.add(u);
+			}
+		}catch(Exception e){
 
-	}
-	public void updateRecipData() {
-
-	}
-	public void deleteRecipData() {
-
+		}finally{
+			try{
+				if(rs != null) rs.close();
+				if(pst != null) pst.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e){
+			}
+		}
+		return al;
 	}
 }
