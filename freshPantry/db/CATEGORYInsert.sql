ecb0b7a291e0f28e06b1995ccc9ffdefad5e99b7
ma-akahoshi <manamiakahoshi@gmail.com>
2018-03-13 14:46:38 +0900

    値データ修正

--- freshPantry/db/CATEGORYInsert.sql
+++ freshPantry/db/CATEGORYInsert.sql
@@ -4,6 +4,5 @@ INSERT INTO fp_category(category_id, category_name) VALUES('002', '魚介類');
 INSERT INTO fp_category(category_id, category_name) VALUES('003', '野菜類');
 INSERT INTO fp_category(category_id, category_name) VALUES('004', '乳製品・卵');
 INSERT INTO fp_category(category_id, category_name) VALUES('005', '加工品');
-INSERT INTO fp_category(category_id, category_name) VALUES('006', '豆類');
-INSERT INTO fp_category(category_id, category_name) VALUES('007', '飲料');
-INSERT INTO fp_category(category_id, category_name) VALUES('008', 'その他');
+INSERT INTO fp_category(category_id, category_name) VALUES('006', '飲料');
+INSERT INTO fp_category(category_id, category_name) VALUES('007', 'その他');
