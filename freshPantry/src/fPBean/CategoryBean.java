ccce8b02ecfa9807a0db9a0753dbf257ada884b7
ma-akahoshi <manamiakahoshi@gmail.com>
2018-03-13 14:41:07 +0900

    SQL取得部分を修正

--- freshPantry/src/fPBean/CategoryBean.java
+++ freshPantry/src/fPBean/CategoryBean.java
@@ -1,17 +1,16 @@
 package fPBean;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
 /**
  * @author 中山佳代子
  * @version 1.0
+ * 赤星
  *
  */
 public class CategoryBean {
@@ -41,17 +40,18 @@ public class CategoryBean {
 
 	public static ArrayList<CategoryBean> getCategoryIdList() throws SQLException {
 		ArrayList<CategoryBean> al = new ArrayList<CategoryBean>();
-		InitialContext ic = null;
-		DataSource ds = null;
 		Connection con = null;
 		PreparedStatement pst = null;
 		ResultSet rs = null;
 		String sql = null;
 
 		try{
-			ic = new InitialContext();
-			ds = (DataSource)ic.lookup("java:comp/env/mysql");
-			con = ds.getConnection();
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch  (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root","root");
 			sql = "SELECT * FROM FP_CATEGORY ORDER BY category_id ASC";
 			pst = con.prepareStatement(sql);
 			rs = pst.executeQuery();
@@ -63,7 +63,7 @@ public class CategoryBean {
 				al.add(c);
 			}
 		}catch(Exception e){
-
+			e.printStackTrace();
 		}finally{
 			try{
 				if(rs != null) rs.close();
@@ -71,6 +71,7 @@ public class CategoryBean {
 				if(con != null) con.close();
 			}
 			catch(Exception e){
+				e.printStackTrace();
 			}
 		}
 		return al;
--- freshPantry/src/fPBean/UnitBean.java
+++ freshPantry/src/fPBean/UnitBean.java
@@ -1,17 +1,16 @@
 package fPBean;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
 /**
  * @author 中山佳代子
  * @version 1.0
+ * 赤星
  *
  */
 public class UnitBean {
@@ -41,17 +40,18 @@ public class UnitBean {
 
 	public static ArrayList<UnitBean> getUnitIdList() throws SQLException {
 		ArrayList<UnitBean> al = new ArrayList<UnitBean>();
-		InitialContext ic = null;
-		DataSource ds = null;
-		Connection con = null;
 		PreparedStatement pst = null;
+		Connection con = null;
 		ResultSet rs = null;
 		String sql = null;
 
 		try{
-			ic = new InitialContext();
-			ds = (DataSource)ic.lookup("java:comp/env/mysql");
-			con = ds.getConnection();
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch  (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root","root");
 			sql = "SELECT * FROM FP_UNIT ORDER BY unit ASC";
 			pst = con.prepareStatement(sql);
 			rs = pst.executeQuery();
