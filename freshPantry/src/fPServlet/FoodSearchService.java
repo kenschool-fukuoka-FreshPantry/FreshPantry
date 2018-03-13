9cea84026df48642a88ab9104e1580ce4a842448
y-morikawa <twin_peaks@live.jp>
2018-03-13 11:39:07 +0900

    3.13 検索サービス（未完成）　森川

---
+++ freshPantry/src/fPServlet/FoodSearchService.java
@@ -0,0 +1,58 @@
+package fPServlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fPBean.FoodBean;
+
+/**
+ * Servlet implementation class FoodSearchService
+ */
+@WebServlet("/FoodSearchService")
+public class FoodSearchService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FoodSearchService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		request.setCharacterEncoding("UTF-8");
+
+		//jspに入力された情報を取得
+		String food_name = (String)request.getParameter("food_name");
+
+		//beanをインスタンス化、値のセット
+		FoodBean fb = new FoodBean();
+		fb.setFoodName(food_name);
+
+		request.setAttribute("foodData", fb);
+
+
+		request.getRequestDispatcher("FoodData.jsp").forward(request, response);
+
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
