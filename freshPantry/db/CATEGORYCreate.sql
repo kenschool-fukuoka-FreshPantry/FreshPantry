b3ba74df0195d5a6f89f3121b6999633b6e6bb5e
ma-akahoshi <manamiakahoshi@gmail.com>
2018-03-13 14:23:47 +0900

    テーブル頭にFP設定

--- freshPantry/db/CATEGORYCreate.sql
+++ freshPantry/db/CATEGORYCreate.sql
@@ -1,4 +1,4 @@
-CREATE TABLE category (
+CREATE TABLE fp_category (
   category_id CHAR(3) ,
   category_name VARCHAR(20) NOT NULL ,
   PRIMARY KEY(category_id)
--- freshPantry/db/FOODCreate.sql
+++ freshPantry/db/FOODCreate.sql
@@ -1,9 +1,9 @@
-CREATE TABLE food (
+CREATE TABLE fp_food (
   food_no INTEGER(10) NOT NULL ,
   nutrient_no INTEGER(2) NOT NULL ,
   nutrient VARCHAR(50) ,
   registration_time TIMESTAMP NOT NULL ,
   update_time TIMESTAMP NOT NULL
   PRIMARY KEY(nutrient_no) ,
-  FOREIGN KEY(food_no) REFERENCES foodmanage(food_no)
+  FOREIGN KEY(food_no) REFERENCES fp_foodmanage(food_no)
 );
