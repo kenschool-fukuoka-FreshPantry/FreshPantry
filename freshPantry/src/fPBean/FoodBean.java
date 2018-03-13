e7e41e73c9fe572712f2a22c2cd915acbb2180bd
ma-akahoshi <manamiakahoshi@gmail.com>
2018-03-13 09:44:30 +0900

    Bean修正分作成 aka

--- freshPantry/src/fPBean/FoodBean.java
+++ freshPantry/src/fPBean/FoodBean.java
@@ -1,7 +1,15 @@
 package fPBean;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  * @author 中山佳代子
+ * @version 1.0
+ * 赤星
  *
  */
 public class FoodBean {
@@ -15,8 +23,7 @@ public class FoodBean {
 	private String expiration_date;
 	private String category_id;
 	private String category_name;
-	private int nutrient_no;
-	private String nutrient;
+	private ArrayList<FoodNutrientBean> nutrientList;
 
 	/**  ゲッターの定義  **/
 	public int getFoodNo() {
@@ -34,10 +41,10 @@ public class FoodBean {
 	public String getUnitName() {
 		return unit_name;
 	}
-	public String getPurchaseDate() {
+	public String getExpirationDate() {
 		return purchase_date;
 	}
-	public String getExpirationDate() {
+	public String getPurchaseDate() {
 		return expiration_date;
 	}
 	public String getCategoryId() {
@@ -46,20 +53,9 @@ public class FoodBean {
 	public String getCategoryName() {
 		return category_name;
 	}
-
-	/** ここは今から作成する予定 S**/
-	public void getNutrientList() {
-
+	public ArrayList<FoodNutrientBean> getNutrientList() {
+		return nutrientList;
 	}
-	/** ここは今から作成する予定 **/
-
-	public int getNutrientNo() {
-		return nutrient_no;
-	}
-	public String getNutrient() {
-		return nutrient;
-	}
-
 
 	/**  セッターの定義  **/
 	public void setFoodNo(int food_no) {
@@ -77,10 +73,10 @@ public class FoodBean {
 	public void setUnitName(String unit_name) {
 		this.unit_name = unit_name;
 	}
-	public void setPurchaseDate(String purchase_date) {
+	public void setExpirationDate(String purchase_date) {
 		this.purchase_date = purchase_date;
 	}
-	public void setExpirationDate(String expiration_date) {
+	public void setPurchaseDate(String expiration_date) {
 		this.expiration_date = expiration_date;
 	}
 	public void setCategoryId(String category_id) {
@@ -89,30 +85,285 @@ public class FoodBean {
 	public void setCategoryName(String category_name) {
 		this.category_name = category_name;
 	}
-	public void setNutrientNo(int nutrient_no) {
-		this.nutrient_no = nutrient_no;
-	}
-	public void setNutrient(String nutrient) {
-		this.nutrient = nutrient;
+	public void setNutrientList(ArrayList<FoodNutrientBean> nutrientList) {
+		this.nutrientList = nutrientList;
 	}
 
+	/**
+	 *  登録メソッド
+	 *
+	 **/
+	public static void addFoodData(FoodBean foodBean) {
 
-	/** ここから先は今から作成する予定 **/
-	public void setNutrientList() {
+		Connection con = null;
+		Statement st = null;
+		String sql = "";
+		ResultSet rs= null;
+		int setFoodNo = 0;
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch  (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// 食材Noを取得する。
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+			st = con.createStatement();
+			sql ="SELECT MAX(food_no) FROM FOODMANAGE";
+			rs = st.executeQuery(sql);
+
+			if(rs.next()){
+				setFoodNo = rs.getInt("food_no") + 1;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				st.close();
+				con.close();
+			} catch  (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+			st = con.createStatement();
+			sql ="INSERT INTO FOODMANAGE("
+					+ "category_id,"
+					+ "food_no,"
+					+ "food_name,"
+					+ "expiration_date,"
+					+ "purchase_date,"
+					+ "quantity,"
+					+ "unit,"
+					+ "registration_time,"
+					+ "update_time)"
+					+ "VALUES('"
+					+ foodBean.getCategoryId() + "',"
+					+ setFoodNo  + ",'"
+					+ foodBean.getFoodName()  + "','"
+					+ foodBean.getExpirationDate()  + "','"
+					+ foodBean.getPurchaseDate()  + "','"
+					+ foodBean.getQuantity()  + "','"
+					+ foodBean.getUnit()  + "',"
+					+ "current_timestamp()" + ","
+					+ "'0000-00-00 00:00:00')";
+			st.executeUpdate(sql);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				st.close();
+				con.close();
+			} catch  (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			// 1件に紐づく栄養素複数県を登録
+			ArrayList<FoodNutrientBean> foodNutrientList = foodBean.getNutrientList();
+
+			int i = 1;
+			for(FoodNutrientBean fnBeanData : foodNutrientList){
+
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+				st = con.createStatement();
+				sql ="INSERT INTO FOOD("
+						+ "food_no,"
+						+ "nutrient_no,"
+						+ "nutrient,"
+						+ "registration_time,"
+						+ "update_time)"
+						+ "VALUES('"
+						+ setFoodNo  + ",'"
+						+ i  + "','"
+						+ fnBeanData.getNutrient()  + "','"
+						+ "current_timestamp()" + ","
+						+ "'0000-00-00 00:00:00')";
+				st.executeUpdate(sql);
+				i++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				st.close();
+				con.close();
+			} catch  (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
-	/** ここから先は今から作成する予定 **/
 
+	public static FoodBean getFoodData(int foodNo) {
+		FoodBean resultFoodBean = new FoodBean();
+		ArrayList<FoodNutrientBean> fnBeenList = new ArrayList<FoodNutrientBean>();
 
+		Connection con = null;
+		Statement st = null;
+		String sql = "";
+		ResultSet rs= null;
 
-	/** ここから先は今から作成する予定 **/
-	public static void addFoodData() {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch  (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
+		try {
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+				st = con.createStatement();
+				sql = "SELECT "
+						+ "fm.category_id AS category_id,"
+						+ "ca.category_name AS category_name,"
+						+ "fm.food_no AS food_no,"
+						+ "fm.food_name AS food_name,"
+						+ "fm.expiration_date AS expiration_date,"
+						+ "fm.purchase_date AS purchase_date,"
+						+ "fm.quantity AS quantity,"
+						+ "fm.unit AS unit,"
+						+ "un.unit_name AS unit_name,"
+						+ "fo.nutrient AS nutrient"
+						+ " FROM FOODMANAGE fm "
+						+ "INNER JOIN UNIT un USEING(unit) "
+						+ "INNER JOIN CATEGORY ca USEING(category_id) "
+						+ "INNER JOIN FOOD fo USEING(food_no) "
+						+ " WHERE fm.food_no = " + foodNo + ";"
+						;
+				rs = st.executeQuery(sql);
+
+			if(rs.next()){
+
+				resultFoodBean.setCategoryId(rs.getString("category_id"));
+				resultFoodBean.setCategoryName(rs.getString("category_name"));
+				resultFoodBean.setFoodNo(rs.getInt("food_no"));
+				resultFoodBean.setFoodName(rs.getString("food_name"));
+
+				if (rs.getDate("expiration_date") != null){
+					resultFoodBean.setExpirationDate(rs.getDate("expiration_date").toString());
+				}
+				if (rs.getDate("purchase_date") != null){
+					resultFoodBean.setPurchaseDate(rs.getDate("purchase_date").toString());
+				}
+				resultFoodBean.setQuantity(rs.getInt("quantity"));
+				resultFoodBean.setUnit(rs.getString("unit"));
+				resultFoodBean.setUnitName(rs.getString("unit_name"));
+
+				FoodNutrientBean fnBeen = new FoodNutrientBean();
+				fnBeen.setNutrient(rs.getString("nutrient"));
+				fnBeenList.add(fnBeen);
+			}
+
+			resultFoodBean.setNutrientList(fnBeenList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				rs.close();
+				st.close();
+				con.close();
+			} catch  (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return resultFoodBean;
 	}
-	public static void getFoodData() {
 
-	}
-	public static void getFoodList() {
+	public static ArrayList<FoodBean> getFoodList(String categryId) {
+
+		ArrayList<FoodBean> resultFoodBeanlist = new ArrayList<FoodBean>();
+		Connection con = null;
+		Statement st = null;
+		String sql = "";
+		ResultSet rs= null;
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch  (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// カテゴリIDがなければ食材全検索
+		try {
+			if (categryId == null || categryId.isEmpty()){
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+				st = con.createStatement();
+				sql = "SELECT "
+						+ "fm.category_id AS category_id,"
+						+ "ca.category_name AS category_name,"
+						+ "fm.food_no AS food_no,"
+						+ "fm.food_name AS food_name,"
+						+ "fm.expiration_date AS expiration_date,"
+						+ "fm.purchase_date AS purchase_date,"
+						+ "fm.quantity AS quantity,"
+						+ "fm.unit AS unit,"
+						+ "un.unit_name AS unit_name"
+						+ " FROM FOODMANAGE fm "
+						+ "INNER JOIN UNIT un USEING(unit) "
+						+ "INNER JOIN CATEGORY ca USEING(category_id) "
+						+ " ORDER BY fm.food_name ASC ;"
+						;
+				rs = st.executeQuery(sql);
+
+			}else{
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+				st = con.createStatement();
+				sql = "SELECT "
+						+ "fm.category_id AS category_id,"
+						+ "ca.category_name AS category_name,"
+						+ "fm.food_no AS food_no,"
+						+ "fm.food_name AS food_name,"
+						+ "fm.expiration_date AS expiration_date,"
+						+ "fm.purchase_date AS purchase_date,"
+						+ "fm.quantity AS quantity,"
+						+ "fm.unit AS unit,"
+						+ "un.unit_name AS unit_name"
+						+ " FROM FOODMANAGE fm "
+						+ "INNER JOIN UNIT un USEING(unit) "
+						+ "INNER JOIN CATEGORY ca USEING(category_id) "
+						+ " WHERE fm.category_id = '" + categryId + "'"
+						+ " ORDER BY fm.food_name ASC ;"
+						;
+				rs = st.executeQuery(sql);
+			}
+
+			if(rs.next()){
+				FoodBean resultFoodBeanData = new FoodBean();
+				resultFoodBeanData.setCategoryId(rs.getString("category_id"));
+				resultFoodBeanData.setCategoryName(rs.getString("category_name"));
+				resultFoodBeanData.setFoodNo(rs.getInt("food_no"));
+				resultFoodBeanData.setFoodName(rs.getString("food_name"));
+
+				if (rs.getDate("expiration_date") != null){
+					resultFoodBeanData.setExpirationDate(rs.getDate("expiration_date").toString());
+				}
+				if (rs.getDate("purchase_date") != null){
+					resultFoodBeanData.setPurchaseDate(rs.getDate("purchase_date").toString());
+				}
+				resultFoodBeanData.setQuantity(rs.getInt("quantity"));
+				resultFoodBeanData.setUnit(rs.getString("unit"));
+				resultFoodBeanData.setUnitName(rs.getString("unit_name"));
+				resultFoodBeanlist.add(resultFoodBeanData);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				rs.close();
+				st.close();
+				con.close();
+			} catch  (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return resultFoodBeanlist;
 
 	}
 	public static void updateFoodData() {
---
+++ freshPantry/src/fPBean/FoodNutrientBean.java
@@ -0,0 +1,29 @@
+package fPBean;
+
+public class FoodNutrientBean {
+
+	private int nutrientNo;
+	private String nutrient;
+
+	public FoodNutrientBean() {
+		super();
+	}
+
+	public int getNutrientNo() {
+		return nutrientNo;
+	}
+
+	public void setNutrientNo(int nutrientNo) {
+		this.nutrientNo = nutrientNo;
+	}
+
+	public String getNutrient() {
+		return nutrient;
+	}
+
+	public void setNutrient(String nutrient) {
+		this.nutrient = nutrient;
+	}
+
+
+}
