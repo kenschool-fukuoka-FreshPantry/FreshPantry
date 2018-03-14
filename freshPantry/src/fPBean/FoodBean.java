ba7b85417078fc74ebc2e2c5c7466586ce256508
y-morikawa <twin_peaks@live.jp>
2018-03-14 13:44:49 +0900

    3.14 FoodBean検索項目完成版　森川

--- freshPantry/src/fPBean/FoodBean.java
+++ freshPantry/src/fPBean/FoodBean.java
@@ -390,7 +390,7 @@ public class FoodBean {
 		}
 		try{
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root","root");
-			sql = "SELECT * FROM FP_FOODMANAGE INNER JOIN CATEGORY USING (category_id) INNER JOIN UNIT USING(unit)"
+			sql = "SELECT * FROM FP_FOODMANAGE INNER JOIN FP_CATEGORY USING (category_id) INNER JOIN FP_UNIT USING(unit)"
 					+ " WHERE food_name like '%" + food_name + "%'"
 					+ " ORDER BY FP_FOODMANAGE.food_name ASC ;";
 			pst = con.prepareStatement(sql);
